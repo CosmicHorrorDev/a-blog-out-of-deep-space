6acc89fe07b66ded4443150ec8b9968f0d48957b
Cosmic Horror <CosmicHorrorDev@pm.me>
2025-07-05 05:24:03 -0600

    feat: spruce up cli arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, net::Ipv4Addr};
+use std::{array, env, net::Ipv4Addr, process};
 
 use a_blog_out_of_deep_space::router;
 use tokio::net::TcpListener;
@@ -8,6 +8,7 @@ use tracing_subscriber::{EnvFilter, filter::LevelFilter, fmt, prelude::*};
 // TODO: strip exif data off of images?
 #[tokio::main]
 async fn main() {
+    // setup logging
     tracing_subscriber::registry()
         .with(fmt::layer())
         .with(
@@ -19,11 +20,25 @@ async fn main() {
         )
         .init();
 
+    // parse cli args
     let mut args = env::args();
-    let _bin = args.next();
-    let dir_to_serve = args.next().unwrap();
+    let args = array::from_fn(|_| args.next());
+    let dir_to_serve = match args {
+        [Some(_), Some(dir), None] if !["-h", "--help", "help"].contains(&&*dir) => dir,
+        [None, ..] => panic!("Somehow arg0 is unset...?"),
+        [Some(bin), ..] => {
+            eprintln!(
+                "Usage: {bin} <DIR_TO_SERVE>\n\
+                \n\
+                Arguments:\n  \
+                <DIR_TO_SERVE>  Directory containing a config file and files to serve"
+            );
+            process::exit(1);
+        }
+    };
     tracing::info!("Loading {dir_to_serve}...");
 
+    // launch server
     let app = router(dir_to_serve.into());
     let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, 8080))
         .await
